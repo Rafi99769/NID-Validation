f8d2d24ccd0faf111afa57f82e3d59f5ddd0cc95
SummerXero <ahnafhossainrafi@gmail.com>
2019-04-07 14:48:22 +0600

    static files in gitignore

--- conf/settings.py
+++ conf/settings.py
@@ -27,9 +27,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Application definition
 
